67b2e48d17a4633e4324e8313a6e44317d8ac180
Erjankydyrov <erjankydyrov29@gmail.com>
2021-02-26 16:00:01 +0600

    corrected some detalis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import List from './components/List';
+import OrderedList from './components/OrderedList';
+import Root from './components/Root';
+import SquareList from './components/SquareList';
+import Table from './components/Table';
+import Wealth from './components/Wealth';
 
 function App() {
+  let richPeople = ["Elon Musk", "Jeff Besos", "Bill Gates"];
+
+  let peopleWealth = [
+    {name: "Elon Musk", wealth: 19283712 },
+    {name: "Jeff Besos", wealth: 19283712 },
+    {name: "Bill Gates", wealth: 19283712 }
+  ]
+
+  let numbers = [4, 6, 1, 9, 2]
+
   return (
     <div className="App">
-      
+      <List items={richPeople} />
+      <OrderedList items={richPeople} />
+      <SquareList items={richPeople} />
+      <Wealth items={peopleWealth} />
+      <Table items={peopleWealth} />
+      <Root items={numbers} />
     </div>
   );
 }
