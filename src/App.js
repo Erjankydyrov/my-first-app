ca629eb925525e938ec0e8c653c9fb73b185a972
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-02 23:33:33 +0600

    corected some problems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import InputsLi from './components/InputsLi';
 
 function App() {
 
   return (
     <div className="App">
-      
+      <InputsLi />
     </div>
   );
 }
