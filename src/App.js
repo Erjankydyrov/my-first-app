5dc2556bd0e584ca04a95e2234588f5b1d5b76ae
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-03 15:57:47 +0600

    App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import InputLength from './components/InputLength';
 import Colors from './components/Colors';
 import InputRange from './components/InputRange';
 import DollarsInput from './components/DollarsInput';
+import FarCell from './components/FarCell';
+import SizeInput from './components/SizeInput';
+import ColorText from './components/ColorText';
+import ColorSliders from './components/ColorSliders';
 
 function App() {
 
+  let FontSize = {}
+
   return (
     <div className="App">
+      <h3>Example1</h3>
       <InputLength value = "text"/>
+      <h3>Example2</h3>
       <Colors value = "#f56b6b"/>
+      <h3>Example3</h3>
       <InputRange min = {0} max = {10} value = {0}/>
+      <h3>Example4</h3>
       <DollarsInput value = {100}/>
+      <h3>Example5</h3>
+      <FarCell valueC = {50} valueF = {10}/>
+      <h3>Example6</h3>
+      <SizeInput value = {19} />
+      <h3>Example7</h3>
+      <ColorText value = "#00ff33"/>
+      <h3>Example7</h3>
+      <ColorSliders />
     </div>
   );
 }
