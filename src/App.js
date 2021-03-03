4ad84082adcef113df37b5d6996b8b5f94ad4ed3
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-03 14:57:26 +0600

    added new coponents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import InputLength from './components/InputLength';
+import Colors from './components/Colors';
+import InputRange from './components/InputRange';
+import DollarsInput from './components/DollarsInput';
 
 function App() {
 
   return (
     <div className="App">
-      <InputLength />
+      <InputLength value = "text"/>
+      <Colors value = "#f56b6b"/>
+      <InputRange min = {0} max = {10} value = {0}/>
+      <DollarsInput value = {100}/>
     </div>
   );
 }
