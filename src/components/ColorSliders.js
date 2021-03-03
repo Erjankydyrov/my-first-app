67f3f9768913feccd447ae2efc78eb277c043d9f
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-03 15:56:42 +0600

    added ColorSliders

---
+++ src/components/ColorSliders.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function ColorSliders({ valueA, valueG, valueB }) {
+    const [number1, setNumber1] = useState(valueA ?? "");
+    const [number2, setNumber2] = useState(valueG ?? "");
+    const [number3, setNumber3] = useState(valueB ?? "");
+    return (
+        <div className="ColorSliders" style = {{backgroundColor: `rgb(${number1}, ${number2}, ${number3})`, padding: 10 + "px"}}>
+            <input
+                type="range"
+                min = {0}
+                max = {255}
+                value = {number1}
+                onInput={(event) => setNumber1(event.target.value)} />
+            <br></br>
+            <input
+                type="range"
+                min = {0}
+                max = {255}
+                value = {number2}
+                onInput={(event) => setNumber2(event.target.value)} />
+                <br></br>
+            <input
+                type="range"
+                min = {0}
+                max = {255}
+                value = {number3}
+                onInput={(event) => setNumber3(event.target.value)} />
+        </div>
+    );
+}
+export default ColorSliders;
