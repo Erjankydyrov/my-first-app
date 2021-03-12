201b393171038e4f4a5966235745529177fa1c97
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-12 13:47:10 +0600

    corected app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import InputLength from './components/InputLength';
-import Colors from './components/Colors';
-import InputRange from './components/InputRange';
-import DollarsInput from './components/DollarsInput';
-import FarCell from './components/FarCell';
-import SizeInput from './components/SizeInput';
-import ColorText from './components/ColorText';
-import ColorSliders from './components/ColorSliders';
-import Rotate from './components/Rotate';
-import Reverse from './components/Reverse';
-import UlList from './components/UlList';
-import MapUl from './components/MapUl';
-import ForOf from './components/ForOf';
+import "./App.css";
+import PersonList from "./components/PersonList/PersonList";
 
 function App() {
-
-  let list = ["banana", "mino", "apple", "Limon", "Popaia"];
-  let list1 = ["banana", "mino", "apple", "Limon", "Popaia", "kiwi", "potato"];
-
+  const persons = [
+    { name: "Elon Baike", age: 47, hobby: "Ice skating, Eating donuts, Dancing under the moon" },
+    { name: "Bill Baike", age: 50, hobby: "Saving the world" }
+  ];
   return (
     <div className="App">
-      
+      <PersonList persons={persons} />
     </div>
   );
 }
-
 export default App;
