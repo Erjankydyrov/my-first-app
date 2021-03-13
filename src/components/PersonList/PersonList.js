9eedb805eaad8da60c54956e4f7fb910ca11504d
Erjankydyrov <erjankydyrov29@gmail.com>
2021-03-13 21:40:26 +0600

    added new compenent PersonList

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -1,12 +1,10 @@
-const PersonList = ({persons}) => {
-    let list = persons.map(person => {
-        return (<div>
-            <h1>{person.name}</h1>
-            <h3>{person.age}</h3>
-            <h4>{person.hobby}</h4>
-        </div>)
-    });
-    return <div>{list}</div>
+import Person from "../Person/Person";
+import classes from "./PersonList.module.css";
+const PersonList = ({ persons }) => {
+    return (
+        <div className={classes.PersonList}>
+            {persons.map(person => <Person {...person} />)}
+        </div>
+    );
 }
- 
 export default PersonList;
